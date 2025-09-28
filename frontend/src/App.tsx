e627eb98daeaccd430723196f37669e16a65f10f
Leonardo Oliveira <71794931+LeonardoOliveira96@users.noreply.github.com>
2025-09-28 12:24:50 -0300

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -44,7 +44,12 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter
+        future={{
+          v7_startTransition: true,
+          v7_relativeSplatPath: true,
+        }}
+      >
         <AuthProvider>
           <AuthInitializer>
             <Routes>
